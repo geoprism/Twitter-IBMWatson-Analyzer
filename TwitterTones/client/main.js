58beca7d0adc4d9da61ece8672c5655393d2db5e
Jason Cha <jasonyhcha@gmail.com>
2016-12-16 00:24:24 -0800

    Figured out async problems. Added animation and loading wheel.

--- TwitterTones/client/main.js
+++ TwitterTones/client/main.js
@@ -1,36 +1,47 @@
 
-
+Meteor.startup(function(){
+  Session.set("submitted", false);
+})
 
 Template.searchBar.events({
   'submit .searchForm'(event) {
+    setTimeout(function(){
+      Session.set('submitted', true);
+    }, 400);
+    Session.set('tone', undefined);
+    Session.set('loading', true);
     event.preventDefault();
-    console.log(event.target.searchbar.value);
+    $('.main-title').animate({
+      marginTop:'2%'
+    });
+    $('.search-container').animate({
+      marginTop:'20px'
+    });
+
+    var text = event.target.searchbar.value;
+    Meteor.call('analyzeTone', text, function(error, result){
+      Session.set('tone', result);
+    });
+    Tracker.autorun(function(){
+      if(Session.get('tone') != undefined){
+        Session.set('loading', false);
+      }
+      else{
+
+      }
+    });
   },
 });
 
+Template.graph.helpers({
+  submitted:function(){
+    return Session.get('submitted');
+  },
 
-Template.tweetTest.helpers({
-    tester: function(){
-        Meteor.call('tweetsRequest', 'happy', function(err, result){
-            if (err)
-              console.log(err);
-
-            Session.set('q', result);
-        });
-
-
-
-        // return ReactiveMethod.call("tweetsRequest", "lol");
-        // //var x = ReactiveMethod.call("tweetsRequest", "happy");
-        // //console.log(x);
-        // //return x;
-    },
-
-    getter: function(){
-        console.log(Session.get('q'));
-        return Session.get('q');
-
-    }
-
-
+  loading:function(){
+    return Session.get('loading');
+  },
+  tone:function(){
+    return "Graph goes here.";
+  }
 });
--- TwitterTones/server/methods.js
+++ TwitterTones/server/methods.js
@@ -1,1 +1,28 @@
 //Methods go here.
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
+var tone_analyzer = new ToneAnalyzerV3({
+  username: '30363b01-94ab-43ff-9852-e81718959aaa',
+  password: 'hI47WE4A3eEv',
+  version_date: '2016-05-19'
+});
+
+function analyze(text, callback){
+  tone_analyzer.tone({ text: text },
+    function(err, tone) {
+      if (err)
+        console.log(err);
+      else{
+        callback(null, tone);
+      }
+  });
+}
+
+var analyze = Async.wrap(analyze);
+
+Meteor.methods({
+  analyzeTone:function(text){
+    var response = analyze(text);
+    return response;
+  }
+});
